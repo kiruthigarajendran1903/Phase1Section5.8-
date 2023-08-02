d39d2095ee224f49d1ed56f92b2568df7493adbb
kiruthigarajendran1903 <kiruthiga2807@gmail.com>
2023-08-02 22:27:32 +0530

    initial commit

---
+++ SubjectTeacher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Phase1Section5._8
+{
+    public class SubjectTeacher : Teacher
+    {
+        private SubjectTeacher whichSubject;
+
+        public SubjectTeacher WhichSubject
+        {
+            get { return whichSubject; }
+            set { whichSubject = value; }
+        }
+
+    }
+}
